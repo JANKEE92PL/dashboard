f7e9e8844a0b043759bb43032bccd812237aa9f2
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-08-19 13:04:08 +0200

    create Navbar.svelte and style it using BULMA CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script>
-  let anzahl = 5
+  import "../node_modules/bulma/css/bulma.min.css";
+  import Navbar from "./components/Navbar.svelte";
+  let anzahl = 5;
 </script>
 
-<svelte:head>
-  <title>Dashboard: {anzahl}</title>
-</svelte:head>
-
-<h1>Dashboard</h1>
+<svelte:head><title>Dashboard: {anzahl -1}/{anzahl}</title></svelte:head>
+<Navbar>To-do-Liste: {anzahl -1}/{anzahl}</Navbar>
 
 <style>
   /* your styles go here */
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<section class="hero is-primary welcome is-small">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">Dashboard</h1>
+      <h2 class="subtitle">
+        <slot><!-- optional fallback --></slot>
+      </h2>
+    </div>
+  </div>
+</section>
