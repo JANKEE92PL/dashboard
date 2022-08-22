cfbdafbaefc02d567d7293ea20f45d1e08878b9a
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-08-22 13:59:35 +0200

    add CRUD

--- src/components/Liste.svelte
+++ src/components/Liste.svelte
@@ -6,6 +6,19 @@
   onMount(async () => {
     $aufgaben = await ListeAPI.laden();
   });
+
+  const bearbeiten = (id) => {
+    ListeAPI.speichern($aufgaben);
+  };
+
+  const loeschen = (id) => {
+    const ok = confirm("Soll die Aufgabe wirklich gelöscht werden?");
+
+    if (ok) {
+      $aufgaben = $aufgaben.filter((aufgabe) => aufgabe.id != id);
+      ListeAPI.speichern($aufgaben);
+    }
+  };
 </script>
 
 <div class="container">
@@ -14,13 +27,27 @@
       <div class="column is-one-third">
         <div class="card">
           <header class="card-header">
-            <p class="card-header-title">
+            <button
+              class="delete is-small"
+              on:click={() => loeschen(aufgabe.id)}>Löschen</button
+            >
+            <p class="card-header-title level">
               {aufgabe.titel}
+              <input
+                type="checkbox"
+                on:blur={() => bearbeiten(aufgabe.id)}
+                bind:checked={aufgabe.erledigt}
+              />
             </p>
           </header>
           <div class="card-content">
             <div class="content">
-              {aufgabe.inhalt}
+              <textarea
+                class="textarea"
+                rows="3"
+                on:blur={() => bearbeiten(aufgabe.id)}
+                bind:value={aufgabe.inhalt}
+              />
             </div>
           </div>
         </div>
